d35af44978bca46c51a4816d7ee6441913ce91df
Alexandre Chartrand <chartrandalexandre@gmail.com>
2021-02-18 21:45:32 -0500

    Adding the possibility to Add and Update note in the noteslist.

--- notes-frontend/src/app/pages/note-details/note-details.component.ts
+++ notes-frontend/src/app/pages/note-details/note-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Note } from 'src/app/shared/note.model';
+import { NotesService } from 'src/app/shared/notes.service';
 
 @Component({
   selector: 'app-note-details',
@@ -10,16 +12,39 @@ import { Note } from 'src/app/shared/note.model';
 export class NoteDetailsComponent implements OnInit {
 
   note: Note;
+  noteId: number;
+  IsNewNote: boolean;
 
-  constructor() { }
+  constructor(private notesService: NotesService, private router:Router, private route:ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.note = new Note();
+
+    // We want to find out if we are creating a new note or editing an existant one.
+    this.route.params.subscribe((params: Params) => {
+      this.note = new Note();
+      if (params.id) {
+        this.note = this.notesService.get(params.id);
+        this.noteId = params.id;
+        this.IsNewNote = false;
+      }
+      else {
+        this.IsNewNote = true;
+      }
+    });
   }
 
   onSubmit(form: NgForm) {
-    console.log(form);
-    
+    // Save the note
+    if (this.IsNewNote) {
+      this.notesService.add(form.value);
+    }
+    else {
+      this.notesService.update(this.noteId, form.value.title, form.value.body);
+    }
+    this.router.navigateByUrl('/');
   }
 
+  onCancel() {
+     this.router.navigateByUrl('/');
+  }
 }
