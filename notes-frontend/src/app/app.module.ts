67e42290c7472eda06b47d4af5d550942ce5cc64
Alexandre Chartrand <chartrandalexandre@gmail.com>
2021-02-24 08:15:49 -0500

    Adding animations when adding or deleting card in card list

--- notes-frontend/src/app/app.module.ts
+++ notes-frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { FormsModule } from '@angular/forms';
 
@@ -19,6 +20,7 @@ import { NoteDetailsComponent } from './pages/note-details/note-details.componen
     NoteDetailsComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule,
     FormsModule
