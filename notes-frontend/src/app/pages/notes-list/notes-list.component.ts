9d97980ae370fa08d5e1fa0d5c5dc49cc6b6c579
Alexandre Chartrand <chartrandalexandre@gmail.com>
2021-02-24 17:17:18 -0500

    Adding the filtering behavior on notelist component

--- notes-frontend/src/app/pages/notes-list/notes-list.component.ts
+++ notes-frontend/src/app/pages/notes-list/notes-list.component.ts
@@ -85,17 +85,58 @@ import { NotesService } from 'src/app/shared/notes.service';
 export class NotesListComponent implements OnInit {
 
   notes: Note[] = new Array<Note>();
-  
+  filteredNotes: Note[] = new Array<Note>();
+
   constructor(private notesService: NotesService) { }
 
   ngOnInit(): void {
     // Retreiving all the notes from NoteServices
     this.notes = this.notesService.getAll();
+    this.filteredNotes = this.notes;
+
   }
 
   deleteNote(id: number){
     this.notesService.delete(id);
   }
 
+  filter(query: string) {
+    query = query.toLowerCase().trim();
+    let allResults: Note[] = new Array<Note>();
 
+    // Split up the seach query into individual words
+    let terms: string[] = query.split(' ');
+    
+    // Remove duplicate terms
+    terms = this.removeDuplicates(terms);
+    
+    // Compile all relevant results into the allResults array
+    terms.forEach(term => {
+      let newResults: Note[] = this.findRelevantNotes(term);
+      allResults = [...allResults, ...newResults];
+    });
+    let uniqueResults = this.removeDuplicates(allResults);
+    this.filteredNotes = uniqueResults;
+  }
+
+  private removeDuplicates(arr: Array<any>) : Array<any> {
+    let uniqueResults: Set<any> = new Set<any>();
+    arr.forEach(element => uniqueResults.add(element));
+    return Array.from(uniqueResults);
+  }
+
+  findRelevantNotes(query: string) : Array<Note>{
+    query = query.toLowerCase().trim();
+    let relevantNotes = this.notes.filter(note => {
+      if (note.title && note.title.toLowerCase().includes(query)){
+        return true;
+      }
+      if (note.body && note.body.toLowerCase().includes(query)) {
+        return true;
+      }
+
+      return false;
+    })
+    return relevantNotes;
+  }
 }
